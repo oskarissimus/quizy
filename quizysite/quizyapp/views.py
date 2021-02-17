1e18003cd7241cd4a395929bbf863055b1d32967
Oskar Korczak <oskarissimus@gmail.com>
2021-02-17 23:42:46 +0100

    scoring implemented

--- quizysite/quizyapp/views.py
+++ quizysite/quizyapp/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from .forms import QuestionForm
+from .forms import MultipleQuestionsForm, QuestionForm
 from .question import Question, QuestionList
 
 # Create your views here.
 def quiz(request):
     if request.method == 'GET':
-        question_list = QuestionList.fromopentdbapi(amount=1, category=9, difficulty='easy')
-        q=question_list[0]
-        form = QuestionForm(choices=q.get_answers_as_choice_field_choices(), label=q.question_text)
+        question_list = QuestionList.fromopentdbapi(amount=3, category=9, difficulty='easy')
+        form = MultipleQuestionsForm(question_list)
+
+        skip = True
+        if 'correct_answers_for_questions' not in request.session.keys() or skip:
+            request.session['correct_answers_for_questions'] = {}
+
+        for question in question_list:
+            request.session['correct_answers_for_questions'][question.question_text] =\
+            question.correct_answer
 
         context={'form': form}
-
-
         return render(request,context=context,template_name='quizyapp/quiz_question.html')
 
     else:
-        print(request.POST)
-        return HttpResponse(f"udzielono odpowiedzi")
+        provided_answers= request.POST
+        correct_answers = request.session['correct_answers_for_questions']
+        points = 0
+        for question in provided_answers.keys():
+            if question in correct_answers:
+                if provided_answers[question] == correct_answers[question]:
+                    points += 1
+        return HttpResponse(f"you scored {points} pts")
