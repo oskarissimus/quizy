b998fccc619c9e71b82d5a6a35e76091675d8636
Oskar Korczak <oskarissimus@gmail.com>
2021-02-23 00:22:12 +0100

    created new model with tests

--- quizyapp/models.py
+++ quizyapp/models.py
@@ -4,6 +4,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserPoints(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, on_delete=models.PROTECT) 
     points = models.IntegerField()
 
+class Category(models.Model):
+    name = models.TextField(unique=True)
+
+class Answer(models.Model):
+    class Meta:
+        unique_together = [('text', 'is_correct')]
+    text = models.TextField()
+    is_correct = models.BooleanField()
+
+class Question(models.Model):
+    answers = models.ManyToManyField(Answer)
+    text = models.TextField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+
+
+    Difficulty = models.TextChoices('Difficulty','easy medium hard')
+    difficulty = models.TextField(choices=Difficulty.choices)
---
+++ tests/tests_quizyapp/tests_models.py
@@ -0,0 +1,29 @@
+
+from django.test import TestCase
+from quizyapp.models import Question, Answer, Category
+
+
+class QuestionAndAnswerRelationTests(TestCase):
+    def setUp(self):
+        cat = Category(name="general")
+        cat.save()
+        a = Answer(text="A", is_correct=True)
+        b = Answer(text="B", is_correct=False)
+        c = Answer(text="C", is_correct=False)
+        d = Answer(text="D", is_correct=False)
+        a.save()
+        b.save()
+        c.save()
+        d.save()
+
+        q = Question(text="What is the first letter of alphabet?", category=cat, difficulty='easy')
+        q.save()
+        q.answers.add(a,b,c,d)
+
+    def test_question_is_initialized(self):
+        q = Question.objects.get(text="What is the first letter of alphabet?")
+        self.assertEqual(q.text, "What is the first letter of alphabet?")
+
+    def test_answers_are_initialized(self):
+        q = Question.objects.get(text="What is the first letter of alphabet?")
+        self.assertEqual(q.answers.get(text='A').text, 'A')
